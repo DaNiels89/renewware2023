f56871254f92de5539833c082c6ea08fee258e3d
DaNiels89 <nielssloesen@hotmail.com>
2023-05-15 14:59:35 +0200

    ADDDED Partytown and config forward for GTM

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+        debug: true,
+      }
+    })
+  ]
 });
